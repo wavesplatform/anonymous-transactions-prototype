9ca203761db55bf89e1af880a0051bd5fca7550f
Igor Gulamov <igor.gulamov@gmail.com>
2019-07-23 16:25:32 +0300

    update zcrypto

--- zcrypto/src/binutils.js
+++ zcrypto/src/binutils.js
@@ -1,14 +1,39 @@
-const bigint2buffLe = (x, n) {
+const bigint2buffLe = (x, n) => {
   const b = Buffer.alloc(n);
-  for(let i=0; i<n;i+=4){
-    const c = Number(x & 0xffffffffn)
-    b[i] = c & 0xff;
-    b[i+1] = (c & 0xff00)>>8;
-    b[i+2] = (c & 0xff0000)>>16;
-    b[i+3] = (c & 0xff000000)>>24;
-    x>>=32n;
-    if(x===0n) break;
+  let t = 0;
+  let ti = 4;
+  for (let i = 0; i < n; i++) {
+    if (ti == 4) {
+      t = Number(x & 0xffffffffn)
+      ti = 0;
+      x >>= 32n;
+    }
+    b[i] = t & 0xff;
+    t >>= 8;
+    ti += 1;
+
+    if (x === 0n && t === 0) break;
   }
+  return b;
 }
 
-module.exports = {bigint2buffLe}
+const buff2bigintLe = (b) => {
+  let t = 0;
+  let ti = 0;
+  let res = 0n;
+  for (let i = 0; i < b.length; i++) {
+    t = (t << 8) + b[i];
+    ti += 1;
+    if (ti == 4) {
+      res = (res << 32n) + BigInt(t);
+      t = 0;
+      ti = 0;
+    }
+  }
+  if (ti > 0) {
+    res = (res << BigInt(ti * 8)) + BigInt(t);
+  }
+  return res;
+}
+
+module.exports = { bigint2buffLe, buff2bigintLe }
--- zcrypto/src/ecryption.js
+++ zcrypto/src/ecryption.js
@@ -1,14 +1,39 @@
 const blake2 = require("blake2");
 const babyjubjub = require("./babyJubJub.js");
+const { buff2bigintLe, bigint2buffLe } = require("./binutils.js");
+const { randomBuffer } = require("secure-random");
+const assert = require("assert");
 
-const hash = (v) => {
-  const hasher = blake2.createHash('blake2b', {digestLength: 32});
-  hasher.update(v);
-  return hasher.digest();
+const b2hash = (v) => {
+  const hasher = blake2.createHash('blake2b', { digestLength: 32 });
+  hasher.update(Buffer.concat(v.map(x => bigint2buffLe(x, 32))));
+  return babyjubjub.affine(buff2bigintLe(hasher.digest()));
 }
 
 const ecdh = (pubk, pk) => babyjubjub.mul(pubk, pk)[0];
 
-const encrypt = (message, pubk, pk) {
-  const seed = ecdh(pk, pubk);
+const encrypt = (message, pubk, pk) => {
+  const h = ecdh(pubk, pk);
+  const iv = b2hash(message);
+  return [iv, ...message.map((x, i) => babyjubjub.affine(x + b2hash([h, iv, BigInt(i)])))];
 }
+
+const decrypt = (encmessage, pubk, pk) => {
+  const h = ecdh(pubk, pk);
+  const [iv, ..._message] = encmessage;
+  const message = _message.map((x, i) => babyjubjub.affine(x - b2hash([h, iv, BigInt(i)])))
+  const checksum = iv === b2hash(message);
+  return [checksum, message];
+}
+
+const encryt_test = () => {
+  const pk = babyjubjub.affine(buff2bigintLe(randomBuffer(32)));
+  const pubk = babyjubjub.mul(babyjubjub.G, babyjubjub.affine(buff2bigintLe(randomBuffer(32))));
+  const message = Array(10).fill(0n).map((x, i) => babyjubjub.affine(buff2bigintLe(randomBuffer(32))));
+  const encrypted = encrypt(message, pubk, pk);
+  const decrypted = decrypt(encrypted, pubk, pk);
+  assert.deepEqual(decrypted[1], message, "Decrypted message must be equal to initial message");
+}
+
+module.exports = { encrypt, decrypt, ecdh };
+
