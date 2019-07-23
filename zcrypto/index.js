2eafaf9d9669df104308eb6efceeb5e5c45c0eda
Igor Gulamov <igor.gulamov@gmail.com>
2019-07-23 16:28:53 +0300

    add exported submodules

--- zcrypto/index.js
+++ zcrypto/index.js
@@ -1,3 +1,5 @@
-const babyjubjub = require("./src/babyJubJub.js.js");
+const babyjubjub = require("./src/babyJubJub.js");
+const binutils = require("./src/binutils.js");
+const encryption = require("./src/ecryption.js");
 
-module.exports = {babyJubJub};
+module.exports = { babyjubjub, binutils, encryption };
